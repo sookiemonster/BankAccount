78f5dd1c3b147cfa645fee5cea4fcee938391656
sookiemonster <dsooknanan20@stuy.edu>
2020-10-27 22:00:01 -0400

    Created BankAccount constructor & instance variables. Added test for making the BankAccount object

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,12 @@
 public class BankAccount {
-  
+
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(int id, String ps) {
+    accountID = id;
+    password = ps;
+    balance = 0.00;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,5 @@
 public class Tester {
   public static void main(String[] args) {
-    
+    BankAccount b1 = new BankAccount(123, "Pass");
   }
 }
